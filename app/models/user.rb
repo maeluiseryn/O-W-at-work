0c7a98186f2388aa523aa6e2d42aad9d951f2e05
maeluiseryn <yodajesuis@hotmail.com>
2011-02-27 18:15:59 +0100

    User password recreate

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,50 +6,49 @@ has_one :user_address, :through => :user_profile, :source => :address
 
 attr_accessor :password
 
-attr_accessible :name , :email ,:password , :password_confirmation
+ attr_accessible :name , :email ,:password , :password_confirmation
 
-validates :name, :presence   => true ,
-                   :length     => { :maximum => 50 },
+ validates :name, :presence   => true ,
+                  :length     => { :maximum => 50 },
+                  :uniqueness => true
+
+ validates :email, :presence   => true ,
+                   :format     => { :with => /(\A[\w.]+[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}\z)/i} ,
                    :uniqueness => true
 
-validates :email, :presence   => true ,
-                    :format     => { :with => /(\A[\w.]+[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}\z)/i} ,
-                    :uniqueness => true
+ # email_regex =  /(\A[a-zA-Z0-9]+[@][a-zA-Z]+[.][a-zA-Z]{2,3}\z)/i ou ((\A[\w.]+[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}\z))
 
-  # email_regex =  /(\A[a-zA-Z0-9]+[@][a-zA-Z]+[.][a-zA-Z]{2,3}\z)/i ou ((\A[\w.]+[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}\z))
-
-validates :password, :presence=> true,
+ validates :password, :presence=> true,
                       :confirmation => true,
                       :length => { :within => 8..40 }
-  #validates :salt, :presence=> true
+ #validates :salt, :presence=> true
 
-  before_save :encrypt_password
+ before_save :encrypt_password
 
-  def has_password?(submitted_password)
-    encrypted_password == encrypt(submitted_password)
-  end
+ def has_password?(submitted_password)
+   encrypted_password == encrypt(submitted_password)
+ end
 
-  private
+ private
 
-  def encrypt_password
-    self.salt = make_salt if new_record?
-    self.encrypted_password = encrypt(password)
-  end
+ def encrypt_password
+   self.salt = make_salt if new_record?
+   self.encrypted_password = encrypt(password)
+ end
 
 
-  def encrypt(string)
-    secure_hash("#{salt}--#{string}")
-  end
+ def encrypt(string)
+   secure_hash("#{salt}--#{string}")
+ end
 
-  def make_salt
-    secure_hash("#{Time.now.utc}--#{password}")
-  end
+ def make_salt
+   secure_hash("#{Time.now.utc}--#{password}")
+ end
 
-  def secure_hash(string)
-    Digest::SHA2.hexdigest(string)
-  end
+ def secure_hash(string)
+   Digest::SHA2.hexdigest(string)
+ end
+
 
 
 end
-
-
