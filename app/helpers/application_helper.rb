e27961f1d546427417d294c64078c93eaa66e56f
maeluiseryn <maeluiseryn@maeluiseryn-desktop.(none)>
2011-03-08 16:51:12 +0100

    some helper...title, flash, signed_in?

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,6 +9,18 @@ module ApplicationHelper
      "#{base_title} | #{@title}"
    end
  end
+ def show_flash
+   render :partial => "shared/flash", :locals => {:flash => flash}
 
+ end
+  def show_sign_in_status
+      if signed_in?
+         link_to 'Sign-out' ,:controller =>'sessions' ,:action =>'destroy'
+
+
+      else
+         link_to 'Sign-in' ,:controller =>'sessions' ,:action =>'new'
+
+      end
+  end
 end
-
