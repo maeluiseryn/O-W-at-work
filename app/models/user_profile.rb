9db8138659aaacbb11258b7c0851b09df4c3063e
maeluiseryn <yodajesuis@hotmail.com>
2011-03-14 01:12:14 +0100

    final before dodo

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -4,6 +4,6 @@ class UserProfile < ActiveRecord::Base
   belongs_to :user
 has_one  :address , :as => :place
 has_many :contacts , :as => :contact_ref
-
-
+accepts_nested_attributes_for :contacts
+#Working .contacts_attributes=[ { :genre => 'Fax' ,:description=>'tel2',:contact_data=>'02020202'}, {:description=>'tel', :genre => 'Fax',:contact_data=>'02020202' } ]
 end
