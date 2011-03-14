0b3a4366afa8efa2e59c2764c853f80a57f25854
maeluiseryn <yodajesuis@hotmail.com>
2011-03-14 23:34:22 +0100

    + a note

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -8,5 +8,7 @@ accepts_nested_attributes_for :contacts , :allow_destroy => true
 
 accepts_nested_attributes_for :address
 
+#nested forms fields_for
+# jquery dynamic form ryanb github complex forms exemple
 #Working .contacts_attributes=[ { :genre => 'Fax' ,:description=>'tel2',:contact_data=>'02020202'}, {:description=>'tel', :genre => 'Fax',:contact_data=>'02020202' } ]
 end
