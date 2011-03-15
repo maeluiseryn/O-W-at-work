1286c8ea0779a157785b60d277dfa595ef1d54c0
maeluiseryn <maeluiseryn@maeluiseryn-desktop.(none)>
2011-03-15 16:46:30 +0100

    some user helper show_or_create_profile

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,3 +1,14 @@
 module UsersHelper
+  def profile_show_or_create user
+    if user.has_profile?
+        link_to 'View Profile',user_user_profiles_path(user)
+    else
+        link_to 'Create Profile',new_user_user_profiles_path(user)
+    end
+
+  end
+  def profile_create_or_edit
+    
+  end
 
 end
